11dac95f30c8b64cd52bd5b68b96b68eab68fce6
Einscrew <manel.rodrigo.reis@gmail.com>
2017-12-16 12:26:42 +0000

    palete form

--- clrs.py
+++ clrs.py
@@ -1,10 +1,12 @@
 #! /usr/bin/python3
 
-def palete():
-	f= '\033[48;5;{}m{:3}\033[0m'
-	print(*[f.format(x,x) for x in range(256)], sep='|')
+def palete(code):
+	f= '\033[{};5;{}m{:3}\033[0m'
+	print('', *[f.format(code,x,x) for x in range(16)], sep='|')
+	print('', *[f.format(code,x,x)+'\n'if (x-15)%6==0 else f.format(code,x,x)+'' for x in range(16,232)], sep='|')
+	print('', *[f.format(code,x,x) for x in range(232,256)], sep='|')
 
 
 if __name__ == '__main__':
-	palete()
+	palete(input('insert 38 for foreground colors or 48 for background colors: '))
 
